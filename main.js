4370e18806b1ed7b89bcf9c684e6d39f5f03403f
Fabinho Flauzino <fabioflauzino@hotmail.com>
2021-07-10 02:05:54 -0300

    sobre nós e serviços parcialmente prontos

---
+++ main.js
@@ -0,0 +1,27 @@
+const nav = document.querySelector('#header nav');
+const toggle = document.querySelectorAll('nav .toggle');
+const links = document.querySelectorAll('nav ul li a');
+
+for(const element of toggle){
+    element.addEventListener('click', () => {
+        nav.classList.toggle('show');
+    })
+}
+
+for(const link of links){
+    link.addEventListener('click', () => {
+        nav.classList.remove('show');
+    });
+}
+
+/*Mudar o header no scroll*/
+const header = document.querySelector('#header');
+const navHeight = header.offsetHeight;
+
+window.addEventListener('scroll', e => {
+    if (window.scrollY >= navHeight) {
+        header.classList.add('scroll');
+    } else {
+        header.classList.remove('scroll');
+    }
+})
