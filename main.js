b6b10ed8fb7a18de541cd2a45de4d9abf8a48e1d
Fabinho Flauzino <fabioflauzino@hotmail.com>
2021-07-11 22:04:54 -0300

    parte contato e adição de animação scrollReveal

--- main.js
+++ main.js
@@ -35,3 +35,21 @@ const swiper = new Swiper('.swiper-container', {
     mousewheel: true,
     keyboard: true    
 });
+
+/**ScrolReveal */
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true
+});
+
+scrollReveal.reveal(`
+    #home .image, #home .text,
+    #about .image, #about .text,
+    #services header, #services .card,
+    #testimonials header, #testimonials .testimonial
+    `, {
+        interval: 100
+    }
+)
